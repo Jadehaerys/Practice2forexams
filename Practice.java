3d9158443e8522cb8b261a606ff71191b85efcc1
Jadehaerys <139621600+Jadehaerys@users.noreply.github.com>
2024-01-31 21:27:36 +0800

    wowjava

---
+++ Practice.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Practice {
+
+public static void main(String [] args)
+
+{
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter first number: ");
+double a = sc.nextDouble();
+System.out.print("Enter second number");
+double b = sc.nextDouble();
+
+double sum = a + b;
+double average = sum / 2;
+
+System.out.print("The sum of the two numbers is: ", + sum);
+System.out.print("The average of the two numbers is: ", + average);
+
+
+}
+
+
+
+
+}
